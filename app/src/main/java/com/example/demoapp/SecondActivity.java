23092083ca24d96165b4852aedfc4714d4e43780
Purmah Prashant <prashant.purmah@sdworx.com>
2022-07-16 15:14:53 +0400

    Lecture 13

--- app/src/main/java/com/example/demoapp/SecondActivity.java
+++ app/src/main/java/com/example/demoapp/SecondActivity.java
@@ -1,7 +1,11 @@
 package com.example.demoapp;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ActivityNotFoundException;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
@@ -26,6 +30,7 @@ public class SecondActivity extends AppCompatActivity {
 
         concatenateButton.setOnClickListener(view -> {
             concatenateNamesAndDisplay();
+            shareMessage();
         });
     }
 
@@ -33,5 +38,36 @@ public class SecondActivity extends AppCompatActivity {
         String firstName = firstNameEditText.getText().toString();
         String lastName = lastNameEditText.getText().toString();
         fullNameTextView.setText(firstName + ' ' + lastName);
+    }
+
+    private void shareMessage() {
+        String fullName = fullNameTextView.getText().toString();
+        Intent sendIntent = new Intent(Intent.ACTION_SEND);
+        sendIntent.putExtra(Intent.EXTRA_TEXT, fullName);
+        sendIntent.setType("text/plain");
+
+        AlertDialog alertDialog =  new AlertDialog.Builder(SecondActivity.this).create();
+        alertDialog.setTitle("Send Message");
+        alertDialog.setMessage(fullName);
+        alertDialog.setButton(AlertDialog.BUTTON_POSITIVE, "Ok",
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        try{
+                            startActivity(sendIntent);
+                        } catch (ActivityNotFoundException e) {
+
+                        }
+                    }
+                });
+        alertDialog.setButton(AlertDialog.BUTTON_NEGATIVE, "Cancel",
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        alertDialog.dismiss();
+                    }
+                });
+        alertDialog.show();
+
     }
 }
